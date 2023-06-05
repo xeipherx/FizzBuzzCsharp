053ea5803015c0868ec8e7a0ac883b9dc550b6d3
xeipherx <phantomxh@hotmail.com>
2023-06-05 20:23:26 +0200

    commit fixxbuzz

---
+++ FizzBuzzCSharp/FizzBuzzCSharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FizzBuzzCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 31; i++)
+            {
+                FizzBuzz(i);
+            }
+        }
+
+        public static void FizzBuzz(int nmbr)
+        {
+            string text = "";
+            if (nmbr % 3 == 0)
+            {
+                text += "Fizz";
+            }
+            if (nmbr % 5 == 0)
+            {
+                text += "Buzz";
+            }
+            string output = String.IsNullOrEmpty(text) ? nmbr.ToString() : text;
+            Console.WriteLine(output);
+        }
+    }
+}
